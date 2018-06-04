38d6ed4091ab2d8bda55cabfbf213256b7621b48
jiayiming <example@brother-bsh.com.cn>
2018-06-04 15:50:45 +0800

    保存一下

--- config/express.js
+++ config/express.js
@@ -10,12 +10,29 @@ const express = require("express"),
     passport = require('passport'), //Passport是Node.js的身份验证中间件
     flash = require('connect-flash'); //用于存储临时消息的node模块
 
+
+    
+/*socket.io实时通讯的使用*/
+const socketio = require('socket.io'),
+    http = require('http'),
+    MongoStore = require('connect-mongo')(session); //当作会话数据储存
+
+
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret,
+        store: MongoStore
+    }));
+
 const config = require('./config'); //加载一些配置,比如session的secret
 
 
 module.exports = function() {
     var app = express();
 
+    var server = http.createServer(app);  //使用新的server替换以前的express应用对象
+    var io = socketio.listen(server);   //将soket.io服务附加给server
 
     if(process.env.NODE_ENV === 'development') {
         app.use(morgan('dev'));       //开发环境下启用简单日志功能
@@ -30,22 +47,22 @@ module.exports = function() {
     app.set('view engine', 'ejs');    //设置EJS为express应用的模板引擎
 
     app.use(flash());
-    app.use(session({
-        name: 'mytest',
-        secret: config.sessionSecret,
-        rolling: true,      //强制将每次访问的回话cookies重置到最大时间
-        resave: true,
-        saveUninitialized: false,
-        cookie: { 
-            maxAge: config.minute
-        },
-        store: new redisStore({
-            host: '127.0.0.1',
-            port: '6379',
-            db: 0,
-            pass: '',
-        })
-    }));//设置session配置
+    // app.use(session({
+    //     name: 'mytest',
+    //     secret: config.sessionSecret,
+    //     rolling: true,      //强制将每次访问的回话cookies重置到最大时间
+    //     resave: true,
+    //     saveUninitialized: false,
+    //     cookie: { 
+    //         maxAge: config.minute
+    //     },
+    //     store: new redisStore({
+    //         host: '127.0.0.1',
+    //         port: '6379',
+    //         db: 0,
+    //         pass: '',
+    //     })
+    // }));//设置session配置
 
 
     app.use(passport.initialize()); 
@@ -58,5 +75,5 @@ module.exports = function() {
     require('../app/routes/user.server.routers')(app);
     require('../app/routes/article.server.routes')(app);
 
-    return app;
+    return server;
 }
---
+++ config/socketio.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var config = require('./config'),
+    cookieParser = require('cookie-parser'),
+    passport = require('passport');
+
+module.exports = function (server, io, mongoStore) {
+    io.use(function (socket, next) {
+        cookieParser(config.sessionSecret)(socket.request, {}, function (err) {
+            var sessionId = socket.request.signedCookiesp['connect.sid'];
+
+            mongoStore.get(sessionId, function (err, session) {
+                socket.request.session = session;
+
+                passport.initialize()(socket.request, {}, function () {
+                    if(socket.request.user) {
+                        next(null, true);
+                    } else {
+                        next(new Error('User is not authenticated'), false);
+                    }
+                });
+            });
+        });
+    });
+
+
+    io.on('connection', function (socket) {
+
+    });
+};
